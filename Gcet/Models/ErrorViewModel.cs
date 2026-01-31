6f396d978813476eb27d7eb710f2dcf5d0b1c960
Jaychangani2005 <jaychangani13@gmail.com>
2026-01-31 15:56:06 +0530

    remove unnecessary files

--- Gcet/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Gcet.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
